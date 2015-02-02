180e4f6270618356745f1c90d59b5dc24db8311d
Ramakrishna Makkena <ramakrishnamakkena@ramakrishnasmbp.gateway.pace.com>
2015-02-02 14:00:21 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ramakrishna Makkena on 2/2/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ramakrishna Makkena on 2/2/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
